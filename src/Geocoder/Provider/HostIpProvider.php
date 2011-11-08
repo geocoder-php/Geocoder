faf90ba5ab86f3a02541c964fed88de69d4c4a3b
William DURAND <william.durand1@gmail.com>
2011-11-08 02:27:50 +0100

    Added new provider for HostIp

---
+++ src/Geocoder/Provider/HostIpProvider.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider;
+
+use Geocoder\HttpAdapter\HttpAdapterInterface;
+use Geocoder\Provider\ProviderInterface;
+
+/**
+ * @author William Durand <william.durand1@gmail.com>
+ */
+class HostIpProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getGeocodedData($address)
+    {
+        if ('127.0.0.1' === $address) {
+            return array(
+                'city'      => 'localhost',
+                'region'    => 'localhost',
+                'country'   => 'localhost'
+            );
+        }
+
+        $query = sprintf('http://api.hostip.info/get_xml.php?ip=%s&position=true', $address);
+
+        $content = $this->getAdapter()->getContent($query);
+        $xml = new \SimpleXmlElement($content);
+        $coordinates = $xml
+            ->children('gml', true)
+                ->featureMember
+                ->children('', true)
+                    ->Hostip
+                    ->ipLocation
+                    ->children('gml', true)
+                        ->pointProperty
+                        ->Point
+                        ->coordinates;
+
+        $lngLat = explode(',', $coordinates);
+        $city = $xml
+            ->children('gml', true)
+                ->featureMember
+                ->children('', true)
+                    ->Hostip
+                        ->children('gml', true)
+                        ->name;
+
+        $country = $xml
+            ->children('gml', true)
+                ->featureMember
+                ->children('', true)
+                    ->Hostip
+                    ->countryName;
+
+        return array(
+            'latitude'  => $lngLat[1],
+            'longitude' => $lngLat[0],
+            'city'      => $city,
+            'country'   => $country
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getReversedData(array $coordinates)
+    {
+        throw new \RuntimeException('The HostIpProvider is not able to do reverse geocoding.');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'host_ip';
+    }
+}
