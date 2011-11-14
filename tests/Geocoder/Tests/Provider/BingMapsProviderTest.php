d4e7cba5cd12a72f104bea1e52c1b798f72cb9b6
David Guyon <dguyon@gmail.com>
2011-11-14 19:01:30 +0100

    Introduced Bing Maps API with unit tests

---
+++ tests/Geocoder/Tests/Provider/BingMapsProviderTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Geocoder\Tests\Provider;
+
+use Geocoder\Tests\TestCase;
+
+use Geocoder\Provider\BingMapsProvider;
+
+class BingMapsProviderTest extends TestCase
+{
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testGetGeocodedDataWithNullApiKey()
+    {
+        $provider = new BingMapsProvider($this->getMock('\Geocoder\HttpAdapter\HttpAdapterInterface'), null);
+        $provider->getGeocodedData('foo');
+    }
+
+    public function testGetGeocodedData()
+    {
+        $this->provider = new BingMapsProvider($this->getMockAdapter(), 'api_key');
+        $result = $this->provider->getGeocodedData('foobar');
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['country']);
+    }
+
+    public function testGetGeocodedDataWithNull()
+    {
+        $this->provider = new BingMapsProvider($this->getMockAdapter(), 'api_key');
+        $result = $this->provider->getGeocodedData(null);
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['country']);
+    }
+
+    public function testGetGeocodedDataWithEmpty()
+    {
+        $this->provider = new BingMapsProvider($this->getMockAdapter(), 'api_key');
+        $result = $this->provider->getGeocodedData('');
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['country']);
+    }
+
+    public function testGetGeocodedDataWithLocalhost()
+    {
+        $this->provider = new BingMapsProvider($this->getMockAdapter($this->never()), 'api_key');
+        $result = $this->provider->getGeocodedData('127.0.0.1');
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['zipcode']);
+
+        $this->assertEquals('localhost', $result['city']);
+        $this->assertEquals('localhost', $result['region']);
+        $this->assertEquals('localhost', $result['country']);
+    }
+
+    public function testGetGeocodedDataWithRealAddress()
+    {
+        if (!isset($_SERVER['BINGMAPS_API_KEY'])) {
+            $this->markTestSkipped('You need to configure the BINGMAPS_API_KEY value in phpunit.xml');
+        }
+
+        $this->provider = new BingMapsProvider(new \Geocoder\HttpAdapter\BuzzHttpAdapter(), $_SERVER['BINGMAPS_API_KEY']);
+        $result = $this->provider->getGeocodedData('10 avenue Gambetta, Paris, France');
+
+        $this->assertEquals(48.86321675999999, $result['latitude']);
+        $this->assertEquals(2.3887721299999995, $result['longitude']);
+        $this->assertEquals(75020, $result['zipcode']);
+        $this->assertEquals('Paris', $result['city']);
+        $this->assertEquals('IdF', $result['region']);
+        $this->assertEquals('France', $result['country']);
+    }
+
+    public function testGetReversedData()
+    {
+        $this->provider = new BingMapsProvider($this->getMockAdapter(), 'api_key');
+        $result = $this->provider->getReversedData(array(1, 2));
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['country']);
+    }
+
+    public function testGetReversedDataWithRealCoordinates()
+    {
+        if (!isset($_SERVER['BINGMAPS_API_KEY'])) {
+            $this->markTestSkipped('You need to configure the BINGMAPS_API_KEY value in phpunit.xml');
+        }
+
+        $this->provider = new BingMapsProvider(new \Geocoder\HttpAdapter\BuzzHttpAdapter(), $_SERVER['BINGMAPS_API_KEY']);
+        $result = $this->provider->getReversedData(array(48.86321675999999, 2.3887721299999995));
+
+        $this->assertEquals(48.86321675999999, $result['latitude']);
+        $this->assertEquals(2.3887721299999995, $result['longitude']);
+        $this->assertEquals(75020, $result['zipcode']);
+        $this->assertEquals('Paris', $result['city']);
+        $this->assertEquals('IdF', $result['region']);
+        $this->assertEquals('France', $result['country']);
+    }
+}
