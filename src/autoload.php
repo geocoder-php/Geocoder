acca963723d5e404f64df634e11718d979e5771f
Jérôme Tamarelle <jerome@tamarelle.net>
2011-11-21 13:56:59 +0100

    Update src/autoload.php

--- src/autoload.php
+++ src/autoload.php
@@ -14,7 +14,7 @@ spl_autoload_register(function($className) {
     }
     $fileName = '';
     $namespace = '';
-    if ($lastNsPos = strripos($className, '\\')) {
+    if ($lastNsPos = strpos($className, '\\')) {
         $namespace = substr($className, 0, $lastNsPos);
         $className = substr($className, $lastNsPos + 1);
         $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
