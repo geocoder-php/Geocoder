a5c04b1fd5300e78d283c7a8b040322b37fdf7ae
Markus Bachmann <markus.bachmann@digital-connect.de>
2011-11-24 08:50:17 +0100

    Add memcache adapter

---
+++ src/Geocoder/CacheAdapter/MemcacheAdapter.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\CacheAdapter;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+class MemcacheAdapter implements CacheInterface
+{
+    /**
+     * @var \Memcache
+     */
+    protected $adapter;
+
+    public function __construct(\Memcache $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    /**
+     * Stores a value with a unique key.
+     *
+     * @param string $key   A unique key.
+     * @param \Geocoder\Result\ResultInterface  A result object.
+     */
+    public function store($key, $value)
+    {
+        $this->adapter->set($key, $value);
+    }
+
+    /**
+     * Retrieves a value identified by its key.
+     *
+     * @return \Geocoder\Result\ResultInterface A result object.
+     */
+    public function retrieve($key)
+    {
+        return $this->adapter->get($key) ?: null;
+    }
+
+}
---
+++ tests/Geocoder/Tests/CacheAdapter/MemcacheAdapterTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Geocoder\Tests\CacheAdapter;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+class MemcacheAdapterTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected function setUp()
+    {
+        if ( ! class_exists('\Memcache', false) ) {
+            $this->markTestSkipped('Memcache extension must be loaded');
+        }
+    }
+
+    /**
+     * @dataProvider getCacheData
+     */
+    public function testRetrieveAndStore($key, $value)
+    {
+        $adapter = new \Memcache();
+        $adapter->addserver('127.0.0.1');
+        $cache = new \Geocoder\CacheAdapter\MemcacheAdapter($adapter);
+        $this->assertNull($cache->retrieve($key));
+        $cache->store($key, $value);
+        $this->assertEquals($value, $cache->retrieve($key));
+    }
+
+    static public function getCacheData()
+    {
+        return array(
+            array('1', null),
+            array('2', 'bar'),
+            array('3', new \stdClass()),
+            array('4', array('foo', 'bar')),
+            array('5', 1),
+            array('6', 2.1)
+        );
+    }
+}
