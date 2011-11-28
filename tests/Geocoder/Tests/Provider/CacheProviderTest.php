a03a88b7d5b184128da9d2d8f357a85c865bf578
Markus Bachmann <markus.bachmann@digital-connect.de>
2011-11-28 13:34:42 +0100

    Add cacheprovider

---
+++ tests/Geocoder/Tests/Provider/CacheProviderTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Geocoder\Tests\Provider;
+
+use Geocoder\Provider\CacheProvider;
+use Geocoder\Provider\ProviderInterface;
+use Geocoder\Tests\TestCase;
+
+class CacheProviderTest extends TestCase
+{
+    public function testGetGeocodedDataWithNull()
+    {
+        $this->provider = new HostIpProvider($this->getMockAdapter());
+        $result = $this->provider->getGeocodedData(null);
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['country']);
+    }
+
+    public function testGetGeocodedDataWithEmpty()
+    {
+        $this->provider = new HostIpProvider($this->getMockAdapter());
+        $result = $this->provider->getGeocodedData('');
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['country']);
+    }
+
+    public function testGetGeocodedDataWithAddress()
+    {
+        $this->provider = new HostIpProvider($this->getMockAdapter());
+        $result = $this->provider->getGeocodedData('10 avenue Gambetta, Paris, France');
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['country']);
+    }
+
+    public function testGetGeocodedDataWithLocalhost()
+    {
+        $this->provider = new HostIpProvider($this->getMockAdapter($this->never()));
+        $result = $this->provider->getGeocodedData('127.0.0.1');
+
+        $this->assertArrayNotHasKey('latitude', $result);
+        $this->assertArrayNotHasKey('longitude', $result);
+        $this->assertArrayNotHasKey('zipcode', $result);
+
+        $this->assertEquals('localhost', $result['city']);
+        $this->assertEquals('localhost', $result['region']);
+        $this->assertEquals('localhost', $result['country']);
+    }
+
+    public function testGetGeocodedDataWithRealIp()
+    {
+        $this->provider = new HostIpProvider(new \Geocoder\HttpAdapter\BuzzHttpAdapter());
+        $result = $this->provider->getGeocodedData('88.188.221.14');
+
+        $this->assertEquals(45.5333, $result['latitude']);
+        $this->assertEquals(2.6167, $result['longitude']);
+        $this->assertArrayNotHasKey('zipcode', $result);
+        $this->assertEquals('Aulnat', $result['city']);
+        $this->assertArrayNotHasKey('region', $result);
+        $this->assertEquals('FRANCE', $result['country']);
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedException
+     */
+    public function testGetReverseData()
+    {
+        $this->provider = new HostIpProvider($this->getMockAdapter($this->never()));
+        $this->provider->getReversedData(array(1, 2));
+    }
+}
