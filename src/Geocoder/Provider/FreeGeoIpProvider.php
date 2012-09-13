cbd09f842422031bba6473bbbf4f67c156efd615
oharridge <ollie@sabbaticalsoul.co.uk>
2012-09-13 09:53:24 +0100

    changing the csv to array

--- src/Geocoder/Provider/FreeGeoIpProvider.php
+++ src/Geocoder/Provider/FreeGeoIpProvider.php
@@ -122,73 +122,64 @@ class FreeGeoIpProvider extends AbstractProvider implements ProviderInterface
     protected function getRegionCodes()
     {
 
-        $csv = <<<CSV
-AK,2,ALASKA
-AL,1,ALABAMA
-AR,5,ARKANSAS
-AS,60,AMERICAN SAMOA
-AZ,4,ARIZONA
-CA,6,CALIFORNIA
-CO,8,COLORADO
-CT,9,CONNECTICUT
-DC,11,DISTRICT OF COLUMBIA
-DE,10,DELAWARE
-FL,12,FLORIDA
-GA,13,GEORGIA
-GU,66,GUAM
-HI,15,HAWAII
-IA,19,IOWA
-ID,16,IDAHO
-IL,17,ILLINOIS
-IN,18,INDIANA
-KS,20,KANSAS
-KY,21,KENTUCKY
-LA,22,LOUISIANA
-MA,25,MASSACHUSETTS
-MD,24,MARYLAND
-ME,23,MAINE
-MI,26,MICHIGAN
-MN,27,MINNESOTA
-MO,29,MISSOURI
-MS,28,MISSISSIPPI
-MT,30,MONTANA
-NC,37,NORTH CAROLINA
-ND,38,NORTH DAKOTA
-NE,31,NEBRASKA
-NH,33,NEW HAMPSHIRE
-NJ,34,NEW JERSEY
-NM,35,NEW MEXICO
-NV,32,NEVADA
-NY,36,NEW YORK
-OH,39,OHIO
-OK,40,OKLAHOMA
-OR,41,OREGON
-PA,42,PENNSYLVANIA
-PR,72,PUERTO RICO
-RI,44,RHODE ISLAND
-SC,45,SOUTH CAROLINA
-SD,46,SOUTH DAKOTA
-TN,47,TENNESSEE
-TX,48,TEXAS
-UT,49,UTAH
-VA,51,VIRGINIA
-VI,78,VIRGIN ISLANDS
-VT,50,VERMONT
-WA,53,WASHINGTON
-WI,55,WISCONSIN
-WV,54,WEST VIRGINIA
-WY,56,WYOMING
-CSV;
+        $states = array(
+            'AK' => 2, //ALASKA
+            'AL' => 1, //ALABAMA
+            'AR' => 5, //ARKANSAS
+            'AS' => 60, //AMERICAN SAMOA
+            'AZ' => 4, //ARIZONA
+            'CA' => 6, //CALIFORNIA
+            'CO' => 8, //COLORADO
+            'CT' => 9, //CONNECTICUT
+            'DC' => 11, //DISTRICT OF COLUMBIA
+            'DE' => 10, //DELAWARE
+            'FL' => 12, //FLORIDA
+            'GA' => 13, //GEORGIA
+            'GU' => 66, //GUAM
+            'HI' => 15, //HAWAII
+            'IA' => 19, //IOWA
+            'ID' => 16, //IDAHO
+            'IL' => 17, //ILLINOIS
+            'IN' => 18, //INDIANA
+            'KS' => 20, //KANSAS
+            'KY' => 21, //KENTUCKY
+            'LA' => 22, //LOUISIANA
+            'MA' => 25, //MASSACHUSETTS
+            'MD' => 24, //MARYLAND
+            'ME' => 23, //MAINE
+            'MI' => 26, //MICHIGAN
+            'MN' => 27, //MINNESOTA
+            'MO' => 29, //MISSOURI
+            'MS' => 28, //MISSISSIPPI
+            'MT' => 30, //MONTANA
+            'NC' => 37, //NORTH CAROLINA
+            'ND' => 38, //NORTH DAKOTA
+            'NE' => 31, //NEBRASKA
+            'NH' => 33, //NEW HAMPSHIRE
+            'NJ' => 34, //NEW JERSEY
+            'NM' => 35, //NEW MEXICO
+            'NV' => 32, //NEVADA
+            'NY' => 36, //NEW YORK
+            'OH' => 39, //OHIO
+            'OK' => 40, //OKLAHOMA
+            'OR' => 41, //OREGON
+            'PA' => 42, //PENNSYLVANIA
+            'PR' => 72, //PUERTO RICO
+            'RI' => 44, //RHODE ISLAND
+            'SC' => 45, //SOUTH CAROLINA
+            'SD' => 46, //SOUTH DAKOTA
+            'TN' => 47, //TENNESSEE
+            'TX' => 48, //TEXAS
+            'UT' => 49, //UTAH
+            'VA' => 51, //VIRGINIA
+            'VI' => 78, //VIRGIN ISLANDS
+            'VT' => 50, //VERMONT
+            'WA' => 53, //WASHINGTON
+            'WI' => 55, //WISCONSIN
+            'WV' => 54, //WEST VIRGINIA
+        );
 
-        $lines = explode("\n", $csv);
-        $ret = array();
-
-        foreach ($lines as $line) {
-            $cols = str_getcsv($line);
-            $ret[$cols[0]] = $cols[1];
-        }
-
-        return $ret;
+        return $states;
     }
 
 }
