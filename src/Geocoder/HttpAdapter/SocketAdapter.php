8164a7f7ec7daee22a0c3572c8f653823a9f5184
Stefan Warman <stefan.warman@gmail.com>
2012-10-01 23:12:38 +0100

    Refactored SocketAdapter to simplify testing. Added tests to verify the behaviour of building HTTP requests/responses.

--- src/Geocoder/HttpAdapter/SocketAdapter.php
+++ src/Geocoder/HttpAdapter/SocketAdapter.php
@@ -15,6 +15,9 @@ namespace Geocoder\HttpAdapter;
  */
 class SocketAdapter implements HttpAdapterInterface
 {
+    const MAX_REDIRECTS = 5;
+    private $redirects_remaining = self::MAX_REDIRECTS;
+
     /**
      * {@inheritDoc}
      */
@@ -28,56 +31,103 @@ class SocketAdapter implements HttpAdapterInterface
         $path     = (isset($info['path']) ? $info['path'] : '/');
         $query    = (isset($info['query']) ? '?'.$info['query'] : '');
 
-        try {
-            $handle = fsockopen($hostname, $port, $errno, $errstr, 30);
-        } catch (\Exception $e) {
-            $handle = null;
-        }
+        $socketHandle = $this->createSocket($hostname, $port, 30);
 
-        if (null === $handle) {
-            throw new \RuntimeException(sprintf('Could not connect to socket. (%s)', $errstr));
-        }
-
-        $request = "GET {$path} HTTP/1.1\r\n"
-            . "Host: {$hostname}\r\n"
-            . "Connection: Close\r\n"
-            . "User-Agent: Geocoder PHP-Library\r\n"
-            . "\r\n";
-
-        if (!fwrite($handle, $request)) {
+        if (!fwrite($socketHandle, $this->buildHttpRequest($path, $hostname))) {
             throw new \RuntimeException('Could not send the request');
         }
 
-        $rawHeaders = array();
-        $rawContent = '';
+        $httpResponse = $this->getParsedHttpResponse($socketHandle);
+
+        if ($httpResponse['headers']['status'] === 301 && isset($httpResponse['headers']['location'])) {
+            if(--$this->redirects_remaining){
+                return $this->getContent($httpResponse['headers']['location']);
+            } else {
+                throw new \RuntimeException('Too Many Redirects');
+            }
+        } else {
+            $this->redirects_remaining = self::MAX_REDIRECTS;
+        }
+
+        if ($httpResponse['headers']['status'] !== 200) {
+            throw new \RuntimeException(sprintf('The server return a %s status.', $httpResponse['headers']['status']));
+        }
+
+        return $httpResponse['content'];
+    }
+
+    /**
+     * This method strictly doesn't need to exist but can act as a "seam" for substituting fake sockets in test.
+     * This would require a subclass that overloads the method and returns the fake socket.
+     *
+     * @param string $hostname
+     * @param string $port
+     * @param int $timeout
+     * @return resource
+     * @throws \RuntimeException
+     */
+    protected function createSocket($hostname, $port, $timeout)
+    {
+        $socketHandle = fsockopen($hostname, $port, $errno, $errstr, $timeout) ?: null;
+
+        //verify handle
+        if (null === $socketHandle){
+            throw new \RuntimeException(sprintf('Could not connect to socket. (%s)', $errstr));
+        }
+
+        return $socketHandle;
+    }
+
+    /**
+     * @param string $path
+     * @param string $hostname
+     * @return string
+     */
+    protected function buildHttpRequest($path, $hostname)
+    {
+        $r = array();
+        $r[] = "GET {$path} HTTP/1.1";
+        $r[] = "Host: {$hostname}";
+        $r[] = "Connection: Close";
+        $r[] = "User-Agent: Geocoder PHP-Library";
+        $r[] = "\r\n";
+
+        return implode("\r\n", $r);
+    }
+
+    /**
+     * Given a resource parse the contents into its component parts (headers/contents)
+     *
+     * @param resource $socketHandle
+     * @return array
+     */
+    protected function getParsedHttpResponse($socketHandle)
+    {
+        $httpResponse = array();
+        $httpResponse['headers'] = array();
+        $httpResponse['content'] = '';
+
         $reachedEndOfHeaders = false;
 
-        while (!feof($handle)) {
-            $line = trim(fgets($handle));
+        while (!feof($socketHandle)) {
+            $line = trim(fgets($socketHandle));
             if(!$line){
                 $reachedEndOfHeaders = true;
                 continue;
             }
             if(!$reachedEndOfHeaders) {
                 if (preg_match('@^HTTP/\d\.\d\s*(\d+)\s*.*$@', $line, $matches)) {
-                    $rawHeaders['status'] = (integer) $matches[1];
-                } elseif (preg_match('@^([^:]+): (.+)@', $line, $matches)) {
-                    $rawHeaders[strtolower($matches[1])] = trim($matches[2]);
+                    $httpResponse['headers']['status'] = (integer)$matches[1];
+                }
+                elseif (preg_match('@^([^:]+): (.+)$@', $line, $matches)) {
+                    $httpResponse['headers'][strtolower($matches[1])] = trim($matches[2]);
                 }
             } else {
-                $rawContent .= $line;
+                $httpResponse['content'] .= $line;
             }
         }
 
-        if ($rawHeaders['status'] === 301 && isset($rawHeaders['location'])) {
-            return $this->getContent($rawHeaders['location']);
-        }
-
-        if ($rawHeaders['status'] !== 200) {
-            throw new \RuntimeException(sprintf('The server return a %s status.', $rawHeaders['status']));
-        }
-
-        return $rawContent;
+        return $httpResponse;
     }
 
     /**
