06b94c9e2f2ce2762bbdde00f3d579d60e5dd225
toin0u <dev@sbin.dk>
2012-10-15 13:26:41 +0200

    Added: a new provider: OIORESTProvider

---
+++ src/Geocoder/Provider/OIORESTProvider.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider;
+
+use Geocoder\Exception\UnsupportedException;
+use Geocoder\Provider\ProviderInterface;
+
+/**
+ * @author Antoine Corcy <contact@sbin.dk>
+ */
+class OIORESTProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * @var string
+     */
+    const ENDPOINT_URL = 'http://geo.oiorest.dk/adresser/%s.json';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getGeocodedData($address)
+    {
+        if (filter_var($address, FILTER_VALIDATE_IP)) {
+            throw new UnsupportedException('The OIORESTProvider does not support IP addresses.');
+        }
+
+        // format address
+        $address = preg_replace('/([a-zæøåÆØÅ]+) (\d+), (\d{4}) ([a-zæøåÆØÅ\ ])+/i', '${1},${2},${3}', $address);
+        $address = rawurlencode($address);
+
+        $query = sprintf(self::ENDPOINT_URL, $address);
+
+        return $this->executeQuery($query);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getReversedData(array $coordinates)
+    {
+        throw new UnsupportedException('The OIORESTProvider is not able to do reverse geocoding.');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'oio_rest';
+    }
+
+    /**
+     * @param  string $query
+     * @return array
+     */
+    protected function executeQuery($query)
+    {
+        $content = $this->getAdapter()->getContent($query);
+
+        if (null === $content) {
+            return $this->getDefaults();
+        }
+
+        $data = (array) json_decode($content, true);
+
+        if (empty($data) || false === $data) {
+            return $this->getDefaults();
+        }
+
+        return array(
+            'latitude'     => isset($data['wgs84koordinat']['bredde']) ? $data['wgs84koordinat']['bredde'] : null,
+            'longitude'    => isset($data['wgs84koordinat']['længde']) ? $data['wgs84koordinat']['længde'] : null,
+            'streetNumber' => isset($data['husnr']) ? $data['husnr'] : null,
+            'streetName'   => isset($data['vejnavn']['navn']) ? $data['vejnavn']['navn'] : null,
+            'city'         => isset($data['postnummer']['navn']) ? $data['postnummer']['navn'] : null,
+            'zipcode'      => isset($data['postnummer']['nr']) ? $data['postnummer']['nr'] : null,
+            'cityDistrict' => isset($data['kommune']['navn']) ? $data['kommune']['navn'] : null,
+            'region'       => isset($data['region']['navn']) ? $data['region']['navn'] : null,
+            'regionCode'   => null,
+            'country'      => 'Denmark',
+            'countryCode'  => 'DK',
+            'timezone'     => null
+        );
+    }
+}
---
+++ tests/Geocoder/Tests/Provider/OIORESTProviderTest.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Geocoder\Tests\Provider;
+
+use Geocoder\Tests\TestCase;
+
+use Geocoder\Provider\OIORESTProvider;
+
+class OIORESTProviderTest extends TestCase
+{
+    public function testGetGeocodedDataWithNull()
+    {
+        $this->provider = new OIORESTProvider($this->getMockAdapter());
+        $result = $this->provider->getGeocodedData(null);
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['streetNumber']);
+        $this->assertNull($result['streetName']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['cityDistrict']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['regionCode']);
+        $this->assertNull($result['country']);
+        $this->assertNull($result['countryCode']);
+        $this->assertNull($result['timezone']);
+    }
+
+    public function testGetGeocodedDataWithEmpty()
+    {
+        $this->provider = new OIORESTProvider($this->getMockAdapter());
+        $result = $this->provider->getGeocodedData('');
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['streetNumber']);
+        $this->assertNull($result['streetName']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['cityDistrict']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['regionCode']);
+        $this->assertNull($result['country']);
+        $this->assertNull($result['countryCode']);
+        $this->assertNull($result['timezone']);
+    }
+
+    public function testGetGeocodedDataWithAddress()
+    {
+        $this->provider = new OIORESTProvider($this->getMockAdapter());
+        $result = $this->provider->getGeocodedData('Tagensvej 47, 2200 København N');
+
+        $this->assertNull($result['latitude']);
+        $this->assertNull($result['longitude']);
+        $this->assertNull($result['streetNumber']);
+        $this->assertNull($result['streetName']);
+        $this->assertNull($result['zipcode']);
+        $this->assertNull($result['city']);
+        $this->assertNull($result['cityDistrict']);
+        $this->assertNull($result['region']);
+        $this->assertNull($result['regionCode']);
+        $this->assertNull($result['country']);
+        $this->assertNull($result['countryCode']);
+        $this->assertNull($result['timezone']);
+    }
+
+    public function testGetGeocodedDataWithRealAddressWithSocketAdapter()
+    {
+        $this->provider = new OIORESTProvider(new \Geocoder\HttpAdapter\SocketAdapter());
+        $result = $this->provider->getGeocodedData('Tagensvej 47, 2200 København N');
+
+        $this->assertEquals(55.6999, $result['latitude'], '', 0.0001);
+        $this->assertEquals(12.5527, $result['longitude'], '', 0.0001);
+        $this->assertEquals(47, $result['streetNumber']);
+        $this->assertEquals('Tagensvej', $result['streetName']);
+        $this->assertEquals(2200, $result['zipcode']);
+        $this->assertEquals('København N', $result['city']);
+        $this->assertEquals('København', $result['cityDistrict']);
+        $this->assertEquals('Region Hovedstaden', $result['region']);
+        $this->assertNull($result['regionCode']);
+        $this->assertEquals('Denmark', $result['country']);
+        $this->assertEquals('DK', $result['countryCode']);
+        $this->assertNull($result['timezone']);
+    }
+
+    public function testGetGeocodedDataWithRealAddressWithCurlHttpAdapter()
+    {
+        $this->provider = new OIORESTProvider(new \Geocoder\HttpAdapter\CurlHttpAdapter());
+        $result = $this->provider->getGeocodedData('Lauritzens Plads 1, 9000 Aalborg');
+
+        $this->assertEquals(57.0489, $result['latitude'], '', 0.0001);
+        $this->assertEquals(9.94566, $result['longitude'], '', 0.0001);
+        $this->assertEquals(1, $result['streetNumber']);
+        $this->assertEquals('Lauritzens Plads', $result['streetName']);
+        $this->assertEquals(9000, $result['zipcode']);
+        $this->assertEquals('Aalborg', $result['city']);
+        $this->assertEquals('Aalborg', $result['cityDistrict']);
+        $this->assertEquals('Region Nordjylland', $result['region']);
+        $this->assertNull($result['regionCode']);
+        $this->assertEquals('Denmark', $result['country']);
+        $this->assertEquals('DK', $result['countryCode']);
+        $this->assertNull($result['timezone']);
+    }
+
+    public function testGetGeocodedDataWithRealAddressWithBuzzHttpAdapter()
+    {
+        $this->provider = new OIORESTProvider(new \Geocoder\HttpAdapter\BuzzHttpAdapter());
+        $result = $this->provider->getGeocodedData('St.Blichers Vej 74, 8210 Århus V');
+
+        $this->assertEquals(56.1623, $result['latitude'], '', 0.0001);
+        $this->assertEquals(10.1501, $result['longitude'], '', 0.0001);
+        $this->assertEquals(74, $result['streetNumber']);
+        $this->assertEquals('St.Blichers Vej', $result['streetName']);
+        $this->assertEquals(8210, $result['zipcode']);
+        $this->assertEquals('Aarhus V', $result['city']);
+        $this->assertEquals('Aarhus', $result['cityDistrict']);
+        $this->assertEquals('Region Midtjylland', $result['region']);
+        $this->assertNull($result['regionCode']);
+        $this->assertEquals('Denmark', $result['country']);
+        $this->assertEquals('DK', $result['countryCode']);
+        $this->assertNull($result['timezone']);
+    }
+
+    public function testGetGeocodedDataWithRealAddressWithGuzzleHttpAdapter()
+    {
+        $this->provider = new OIORESTProvider(new \Geocoder\HttpAdapter\GuzzleHttpAdapter());
+        $result = $this->provider->getGeocodedData('Århusgade 80, 2100 København Ø');
+
+        $this->assertEquals(55.7063, $result['latitude'], '', 0.0001);
+        $this->assertEquals(12.5837, $result['longitude'], '', 0.0001);
+        $this->assertEquals(80, $result['streetNumber']);
+        $this->assertEquals('Århusgade', $result['streetName']);
+        $this->assertEquals(2100, $result['zipcode']);
+        $this->assertEquals('København Ø', $result['city']);
+        $this->assertEquals('København', $result['cityDistrict']);
+        $this->assertEquals('Region Hovedstaden', $result['region']);
+        $this->assertNull($result['regionCode']);
+        $this->assertEquals('Denmark', $result['country']);
+        $this->assertEquals('DK', $result['countryCode']);
+        $this->assertNull($result['timezone']);
+    }
+
+    public function testGetGeocodedDataWithRealAddressWithZendHttpAdapter()
+    {
+        $this->provider = new OIORESTProvider(new \Geocoder\HttpAdapter\ZendHttpAdapter());
+        $result = $this->provider->getGeocodedData('Hvenekildeløkken 255, 5240 Odense');
+
+        $this->assertEquals(55.4221, $result['latitude'], '', 0.0001);
+        $this->assertEquals(10.4588, $result['longitude'], '', 0.0001);
+        $this->assertEquals(255, $result['streetNumber']);
+        $this->assertEquals('Hvenekildeløkken', $result['streetName']);
+        $this->assertEquals(5240, $result['zipcode']);
+        $this->assertEquals('Odense NØ', $result['city']);
+        $this->assertEquals('Odense', $result['cityDistrict']);
+        $this->assertEquals('Region Syddanmark', $result['region']);
+        $this->assertNull($result['regionCode']);
+        $this->assertEquals('Denmark', $result['country']);
+        $this->assertEquals('DK', $result['countryCode']);
+        $this->assertNull($result['timezone']);
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedException
+     */
+    public function testGetGeocodedDataWithLocalhost()
+    {
+        $this->provider = new OIORESTProvider($this->getMockAdapter($this->never()));
+        $this->provider->getGeocodedData('127.0.0.1');
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedException
+     */
+    public function testGetGeocodedDataWithIp()
+    {
+        $this->provider = new OIORESTProvider(new \Geocoder\HttpAdapter\BuzzHttpAdapter());
+        $this->provider->getGeocodedData('74.200.247.59');
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedException
+     */
+    public function testGetReverseData()
+    {
+        $this->provider = new OIORESTProvider($this->getMockAdapter($this->never()));
+        $this->provider->getReversedData(array(1, 2));
+    }
+}
