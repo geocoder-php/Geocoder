355f94a89075e3b12d22ded897239c5fa3b0709d
toin0u <dev@sbin.dk>
2012-10-24 22:21:30 +0200

    Updated: tests bootstrap - check cURL and dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,22 @@
 <?php
 
-$loader = require_once __DIR__ . '/../vendor/autoload.php';
+if (!extension_loaded('curl') || !function_exists('curl_init')) {
+    die(<<<EOT
+cURL has to be enabled !
+EOT
+    );
+}
+
+if (!($loader = @include __DIR__ . '/../vendor/autoload.php')) {
+    die(<<<EOT
+You need to install the project dependencies using Composer:
+$ wget http://getcomposer.org/composer.phar
+OR
+$ curl -s https://getcomposer.org/installer | php
+$ php composer.phar install --dev
+$ phpunit
+EOT
+    );
+}
 
 $loader->add('Geocoder\Tests', __DIR__);
