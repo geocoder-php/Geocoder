f74b6ba29aa20f0129d241b9ab6ae68144adf24e
Ronan Guilloux <ronan.guilloux@gmail.com>
2012-11-02 15:03:50 +0100

    code coverage enlarged

--- tests/Geocoder/Tests/Helper/BoundingBoxHelperTest.php
+++ tests/Geocoder/Tests/Helper/BoundingBoxHelperTest.php
@@ -32,6 +32,37 @@ class BoundingBoxHelperTest extends TestCase
         $this->assertEquals($expected, $result);
     }
 
+    public function testGetAngleWithDistancesInMilesAndKm()
+    {
+        $obj = new Geocoded();
+        $obj->fromArray(array('latitude'=>'47.218371', 'longitude'=>'-1.553621'));
+
+        $expected = new Geocoded();
+        // 500 km away from $obj, in the nortEast direction
+        $expected->fromArray(array('latitude'=>'50.291525105654', 'longitude'=>'3.4187817544348'));
+
+
+        $miles = 310.685596; //  = 500 km
+        $km = 500; // = 310.685596 miles
+
+        $resultWithKm = $this->helper->getAngle($obj, 45, $km ,'km');
+        $resultWithMiles = $this->helper->getAngle($obj, 45, $miles,'m');
+
+        // comparing lat/long floating values with a precision of roughly 8 decimal digits
+        $kmLat = sprintf("%01.8F$", $resultWithKm->getLatitude());
+        $kmLong = sprintf("%01.8F$", $resultWithKm->getLongitude());
+
+        $milesLat = sprintf("%01.8F$", $resultWithMiles->getLatitude());
+        $milesLong = sprintf("%01.8F$", $resultWithMiles->getLongitude());
+
+        $this->assertTrue($resultWithKm instanceOf Geocoded);
+        $this->assertTrue($resultWithMiles instanceOf Geocoded);
+        $this->assertEquals($expected, $resultWithKm);
+        $this->assertEquals($kmLat, $milesLat);
+        $this->assertEquals($kmLong, $milesLong);
+
+    }
+
     public function testGetAngleWithZeroDistance()
     {
         $obj = new Geocoded();
