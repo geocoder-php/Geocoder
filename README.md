0ce65b87edf79753b4b30bc5522e5af1850cc540
Andrea Chris Cristaudo <andrea.cristaudo@gmail.com>
2012-11-11 22:58:24 +0100

    Added MaxMindProviders

--- README.md
+++ README.md
@@ -44,9 +44,7 @@ Currently, there are many providers for the following APIs:
 * [IGN OpenLS](http://www.ign.fr/) as Address-Based geocoding provider (exclusively in France);
 * [DataScienceToolkit](http://www.datasciencetoolkit.org/) as IP-Based geocoding provider;
 * [Yandex](http://api.yandex.com.tr/maps/doc/geocoder/desc/concepts/About.xml) as Address-Based geocoding and reverse geocoding provider;
-* [MaxMind web services](http://dev.maxmind.com/geoip/web-services) as IP-Based geocoding providers;
-* [GeoIPs](http://www.geoips.com/developer/geoips-api) as IP-Based geocoding providers;
-* [geoPlugin](http://www.geoplugin.com/webservices) as IP-Based geocoding providers.
+* [MaxMind web services](http://dev.maxmind.com/geoip/web-services) as IP-Based geocoding providers.
 
 
 Installation
@@ -228,17 +226,6 @@ A valid api key is required.
 
 The `MaxMindOmniProvider` is able to geocode **IPv4 addresses** only. This MaxMind provider retrieves timezone too (using another webservice).
 A valid api key is required.
-
-
-### GeoIPsProvider ###
-
-The `GeoIPsProvider` is able to geocode **IPv4 addresses** only.
-A valid api key is required.
-
-
-### GeoPluginProvider ###
-
-The `GeoPluginProvider` is able to geocode **IPv4 addresses and IPv6 addresses** only.
 
 
 You can use one of them or write your own provider. You can also register all providers and decide later.
@@ -498,7 +485,6 @@ Rename the `phpunit.xml.dist` file to `phpunit.xml`, then uncomment the followin
     <!-- <server name="CLOUDMADE_API_KEY" value="YOUR_API_KEY" /> -->
     <!-- <server name="IGN_WEB_API_KEY" value="YOUR_API_KEY" /> -->
     <!-- <server name="MAXMIND_API_KEY" value="YOUR_API_KEY" /> -->
-    <!-- <server name="GEOIPS_API_KEY" value="YOUR_API_KEY" /> -->
 </php>
 ```
 
--- src/Geocoder/Provider/MaxMindOmniProvider.php
+++ src/Geocoder/Provider/MaxMindOmniProvider.php
@@ -10,15 +10,10 @@
 
 namespace Geocoder\Provider;
 
-use Geocoder\Exception\InvalidCredentialsException;
-use Geocoder\Exception\NoResultException;
-use Geocoder\Exception\UnsupportedException;
-use Geocoder\HttpAdapter\HttpAdapterInterface;
-
 /**
  * @author Andrea Cristaudo <andrea.cristaudo@gmail.com>
  */
-class MaxMindOmniProvider extends AbstractProvider implements ProviderInterface
+class MaxMindOmniProvider extends AbstractMaxMindProvider
 {
     /**
      * @var string
@@ -26,54 +21,33 @@ class MaxMindOmniProvider extends AbstractProvider implements ProviderInterface
     const GEOCODE_ENDPOINT_URL = 'http://geoip.maxmind.com/e?l=%s&i=%s';
 
     /**
-     * @var string
+     * @var boolean
      */
-    private $apiKey;
+    const MAXMIND_SUPPORTS_IPV6 = false;
 
     /**
-     * @param \Geocoder\HttpAdapter\HttpAdapterInterface $adapter
-     * @param string                                     $apiKey
+     * @var integer
      */
-    public function __construct(HttpAdapterInterface $adapter, $apiKey)
-    {
-        parent::__construct($adapter, null);
-
-        $this->apiKey = $apiKey;
-    }
+    const MAXMIND_EXPECTED_CHUNKS = 24;
 
     /**
-     * {@inheritDoc}
+     * @param  array $chunks
+     *
+     * @return array
      */
-    public function getGeocodedData($address)
+    protected function mapChunksToArray($chunks)
     {
-        if (null === $this->apiKey) {
-            throw new InvalidCredentialsException('No API Key provided.');
-        }
-
-        if (!filter_var($address, FILTER_VALIDATE_IP)) {
-            throw new UnsupportedException('The MaxMindOmniProvider does not support street addresses.');
-        }
-
-        // This API does not support IPv6
-        if (filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-            throw new UnsupportedException('The MaxMindOmniProvider does not support IPv6 addresses.');
-        }
-
-        if ($address === '127.0.0.1') {
-            return $this->getLocalhostDefaults();
-        }
-
-        $query = sprintf(self::GEOCODE_ENDPOINT_URL, $this->apiKey, $address);
-
-        return $this->executeQuery($query);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getReversedData(array $coordinates)
-    {
-        throw new UnsupportedException('The MaxMindOmniProvider is not able to do reverse geocoding.');
+        return array(
+            'countryCode' => $this->chunkValueOrNull($chunks[0]),
+            'country'     => $this->chunkValueOrNull($chunks[1]),
+            'regionCode'  => $this->chunkValueOrNull($chunks[2]),
+            'region'      => $this->chunkValueOrNull($chunks[3]),
+            'city'        => $this->chunkValueOrNull($chunks[4]),
+            'latitude'    => $this->chunkValueOrNull($chunks[5]),
+            'longitude'   => $this->chunkValueOrNull($chunks[6]),
+            'timezone'    => $this->chunkValueOrNull($chunks[9]),
+            'zipcode'     => $this->chunkValueOrNull($chunks[11]),
+        );
     }
 
     /**
@@ -82,43 +56,5 @@ class MaxMindOmniProvider extends AbstractProvider implements ProviderInterface
     public function getName()
     {
         return 'maxmind_omni';
-    }
-
-    /**
-     * @param  string $query
-     *
-     * @return array
-     */
-    protected function executeQuery($query)
-    {
-        $content = $this->getAdapter()->getContent($query);
-
-        if (null === $content || '' === $content) {
-            throw new NoResultException(sprintf('Could not execute query %s', $query));
-        }
-
-        $data = explode(',', $content);
-
-        if (24 !== count($data)) {
-            throw new NoResultException(sprintf('Could not execute query %s', $query));
-        }
-
-        if (in_array($data[23], array('INVALID_LICENSE_KEY', 'LICENSE_REQUIRED'))) {
-            throw new InvalidCredentialsException('API Key provided is not valid.');
-        } elseif ($data[23] == 'IP_NOT_FOUND') {
-            throw new NoResultException('Could not retrieve informations for the ip address provided.');
-        }
-
-        return array_merge($this->getDefaults(), array(
-            'countryCode' => '' === $data[0] ? null : $data[0],
-            'country'     => '' === $data[1] ? null : $data[1],
-            'regionCode'  => '' === $data[2] ? null : $data[2],
-            'region'      => '' === $data[3] ? null : $data[3],
-            'city'        => '' === $data[4] ? null : $data[4],
-            'latitude'    => '' === $data[5] ? null : $data[5],
-            'longitude'   => '' === $data[6] ? null : $data[6],
-            'timezone'    => '' === $data[9] ? null : $data[9],
-            'zipcode'     => '' === $data[11] ? null : $data[11],
-        ));
     }
 }
