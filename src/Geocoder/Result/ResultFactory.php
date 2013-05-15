6ad36be0c790a1623a7957f5985cd117f0d54757
William DURAND <william.durand1@gmail.com>
2013-05-15 15:57:14 +0200

    Fix BC break by reintroducing the ResultFactory class
    Fixes #235

---
+++ src/Geocoder/Result/ResultFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Result;
+
+/**
+ * For Backward Compatibility purpose only, will
+ * be removed as of 2.0.0. You should **not** use
+ * that class anymore.
+ *
+ * @author William Durand <william.durand1@gmail.com>
+ */
+class ResultFactory extends DefaultResultFactory
+{
+}
