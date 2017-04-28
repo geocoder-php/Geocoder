9953ad910b3260ff66921a7f595df056f87316d0
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-04-28 16:50:35 +0200

    Minor fix

--- tests/Provider/GeoIP2Test.php
+++ tests/Provider/GeoIP2Test.php
@@ -214,7 +214,10 @@ class GeoIP2Test extends TestCase
      */
     public function getGeoIP2AdapterMock($returnValue = '')
     {
-        $mock = $this->getMockBuilder('Geocoder\Adapter\GeoIP2Adapter')->disableOriginalConstructor()->getMock();
+        $mock = $this->getMockBuilder('Geocoder\Adapter\GeoIP2Adapter')
+            ->disableOriginalConstructor()
+            ->setMethods(['getContent'])
+            ->getMock();
 
         if ($returnValue instanceof \Exception) {
             $returnValue = $this->throwException($returnValue);
