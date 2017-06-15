0ebc936deddd7dcf2597a84d537e4f18d8042352
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-15 23:22:53 +0200

    Prepare Mapzen for a separate package (#667)
    * Prepare Mapzen for a separate package
    
    * cs
    
    * Use stable version of integration tests

---
+++ src/Provider/Mapzen/Tests/IntegrationTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\Mapzen\Tests;
+
+use Geocoder\IntegrationTest\ProviderIntegrationTest;
+use Geocoder\Provider\Mapzen\Mapzen;
+use Http\Client\HttpClient;
+
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class IntegrationTest extends ProviderIntegrationTest
+{
+    protected $skippedTests  = [
+        'testGeocodeQuery' => 'We get wrongs cords',
+        'testReverseQueryWithNoResults' => 'We weirdly find stuff here...',
+    ];
+
+    protected $testIpv4 = false;
+    protected $testIpv6 = false;
+
+    protected function createProvider(HttpClient $httpClient)
+    {
+        return new Mapzen($httpClient, $this->getApiKey());
+    }
+
+    protected function getCacheDir()
+    {
+        return __DIR__.'/.cached_responses';
+    }
+
+    protected function getApiKey()
+    {
+        return $_SERVER['MAPZEN_API_KEY'];
+    }
+}
