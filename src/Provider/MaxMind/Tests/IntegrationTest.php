be429cccb49538b92aa11b7838fed449713fe9d4
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-15 23:23:22 +0200

    Prepare maxmind for separate package (#670)
    * Prepare maxmind for separate package
    
    * cs
    
    * Use stable version of integration tests

---
+++ src/Provider/MaxMind/Tests/IntegrationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\MaxMind\Tests;
+
+use Geocoder\IntegrationTest\ProviderIntegrationTest;
+use Geocoder\Provider\MaxMind\MaxMind;
+use Http\Client\HttpClient;
+
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class IntegrationTest extends ProviderIntegrationTest
+{
+    protected $testAddress = false;
+    protected $testReverse = false;
+
+    protected function createProvider(HttpClient $httpClient)
+    {
+        return new MaxMind($httpClient, $this->getApiKey());
+    }
+
+    protected function getCacheDir()
+    {
+        return __DIR__.'/.cached_responses';
+    }
+
+    protected function getApiKey()
+    {
+        return $_SERVER['MAXMIND_API_KEY'];
+    }
+}
