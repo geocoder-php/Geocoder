439423035cca8a9479b866e9daee696210a8e5cb
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-15 23:20:51 +0200

    Prepare GeoIp to separate package (#658)
    * Prepare GeoIp to separate package
    
    * cs
    
    * added travis config
    
    * Skip tests if deps not installed
    
    * Use feature flags
    
    * Use stable version of tests

---
+++ src/Provider/Geoip/Readme.md
@@ -0,0 +1,20 @@
+# Bing Maps Geocoder provider
+[![Build Status](https://travis-ci.org/geocoder-php/geoip-provider.svg?branch=master)](http://travis-ci.org/geocoder-php/geoip-provider)
+[![Latest Stable Version](https://poser.pugx.org/geocoder-php/geoip-provider/v/stable)](https://packagist.org/packages/geocoder-php/geoip-provider)
+[![Total Downloads](https://poser.pugx.org/geocoder-php/geoip-provider/downloads)](https://packagist.org/packages/geocoder-php/geoip-provider)
+[![Monthly Downloads](https://poser.pugx.org/geocoder-php/geoip-provider/d/monthly.png)](https://packagist.org/packages/geocoder-php/geoip-provider)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE)
+
+This is the Geoip provider from the PHP Geocoder. This is a **READ ONLY** repository. See the
+[main repo](https://github.com/geocoder-php/Geocoder) for information and documentation. 
+
+### Install
+
+```bash
+composer require geocoder-php/geoip-provider
+```
+
+### Contribute
+
+Contributions are very welcome! Send a pull request to the [main repository](https://github.com/geocoder-php/Geocoder) or 
+report any issues you find on the [issue tracker](https://github.com/geocoder-php/Geocoder/issues).
--- src/Provider/Geoip/Tests/GeoipTest.php
+++ src/Provider/Geoip/Tests/GeoipTest.php
@@ -12,47 +12,30 @@ declare(strict_types=1);
 
 namespace Geocoder\Provider\Geoip\Tests;
 
+use Geocoder\IntegrationTest\BaseTestCase;
 use Geocoder\Location;
 use Geocoder\Query\GeocodeQuery;
 use Geocoder\Query\ReverseQuery;
-use Geocoder\Tests\TestCase;
 use Geocoder\Provider\Geoip\Geoip;
 
-class GeoipTest extends TestCase
+class GeoipTest extends BaseTestCase
 {
     protected function setUp()
     {
-        parent::setUp();
-
         if (!function_exists('geoip_record_by_name')) {
             $this->markTestSkipped('You have to install GeoIP.');
         }
+    }
+
+    protected function getCacheDir()
+    {
+        return __DIR__.'/.cached_responses';
     }
 
     public function testGetName()
     {
         $provider = new Geoip();
         $this->assertEquals('geoip', $provider->getName());
-    }
-
-    /**
-     * @expectedException \Geocoder\Exception\UnsupportedOperation
-     * @expectedExceptionMessage The Geoip provider does not support street addresses, only IPv4 addresses.
-     */
-    public function testGeocodeWithNull()
-    {
-        $provider = new Geoip();
-        $provider->geocodeQuery(GeocodeQuery::create(null));
-    }
-
-    /**
-     * @expectedException \Geocoder\Exception\UnsupportedOperation
-     * @expectedExceptionMessage The Geoip provider does not support street addresses, only IPv4 addresses.
-     */
-    public function testGeocodeWithEmpty()
-    {
-        $provider = new Geoip();
-        $provider->geocodeQuery(GeocodeQuery::create(''));
     }
 
     /**
