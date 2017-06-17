c4817cb86c0761d2ebad21417a535243a98231d9
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-17 09:35:26 +0200

    cs

--- src/Http/Tests/Provider/AbstractHttpProviderTest.php
+++ src/Http/Tests/Provider/AbstractHttpProviderTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
 namespace Geocoder\Http\Provider\Tests;
 
 use Geocoder\Http\Provider\AbstractHttpProvider;
@@ -16,10 +24,10 @@ class AbstractHttpProviderTest extends TestCase
     }
 }
 
-class DummyProvider extends AbstractHttpProvider {
+class DummyProvider extends AbstractHttpProvider
+{
     public function getHttpClient()
     {
         return parent::getHttpClient();
     }
-
 }
