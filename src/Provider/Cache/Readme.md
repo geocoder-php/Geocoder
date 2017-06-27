34c42fe047220eb2a264b74280a1217e6c26d33f
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-27 13:45:59 +0200

    Adding cache provider (#717)
    * Adding cache provider
    
    * Applied changes from StyleCI
    
    * Bugfix
    
    * Added major version number in cache key
    
    * Be more relaxed with tests

---
+++ src/Provider/Cache/Readme.md
@@ -0,0 +1,22 @@
+# Cache provider
+[![Build Status](https://travis-ci.org/geocoder-php/cache-provider.svg?branch=master)](http://travis-ci.org/geocoder-php/cache-provider)
+[![Latest Stable Version](https://poser.pugx.org/geocoder-php/cache-provider/v/stable)](https://packagist.org/packages/geocoder-php/cache-provider)
+[![Total Downloads](https://poser.pugx.org/geocoder-php/cache-provider/downloads)](https://packagist.org/packages/geocoder-php/cache-provider)
+[![Monthly Downloads](https://poser.pugx.org/geocoder-php/cache-provider/d/monthly.png)](https://packagist.org/packages/geocoder-php/cache-provider)
+[![Code Coverage](https://img.shields.io/scrutinizer/coverage/g/geocoder-php/cache-provider.svg?style=flat-square)](https://scrutinizer-ci.com/g/geocoder-php/cache-provider)
+[![Quality Score](https://img.shields.io/scrutinizer/g/geocoder-php/cache-provider.svg?style=flat-square)](https://scrutinizer-ci.com/g/geocoder-php/cache-provider)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE)
+
+This is the a cache provider from the PHP Geocoder. This is a **READ ONLY** repository. See the
+[main repo](https://github.com/geocoder-php/Geocoder) for information and documentation. 
+
+### Install
+
+```bash
+composer require geocoder-php/cache-provider
+```
+
+### Contribute
+
+Contributions are very welcome! Send a pull request to the [main repository](https://github.com/geocoder-php/Geocoder) or 
+report any issues you find on the [issue tracker](https://github.com/geocoder-php/Geocoder/issues).
---
+++ src/Provider/Cache/Tests/ProviderCacheTest.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\Cache\Tests;
+
+use Geocoder\Model\Address;
+use Geocoder\Model\AddressCollection;
+use Geocoder\Provider\Cache\ProviderCache;
+use Geocoder\Provider\Provider;
+use Geocoder\Query\GeocodeQuery;
+use Geocoder\Query\ReverseQuery;
+use PHPUnit\Framework\TestCase;
+use Psr\SimpleCache\CacheInterface;
+
+/**
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class ProviderCacheTest extends TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|Provider
+     */
+    private $providerMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|CacheInterface
+     */
+    private $cacheMock;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->cacheMock = $this->getMockBuilder(CacheInterface::class)
+            ->setMethods(['get', 'set', 'delete', 'clear', 'setMultiple', 'getMultiple', 'deleteMultiple', 'has'])
+
+            ->getMock();
+
+        $this->providerMock = $this->getMockBuilder(Provider::class)
+            ->setMethods(['getFoo', 'getName', 'geocodeQuery', 'reverseQuery'])
+            ->getMock();
+    }
+
+    public function testName()
+    {
+        $this->providerMock->expects($this->once())
+            ->method('getName')
+            ->willReturn('foo');
+
+        $providerCache = new ProviderCache($this->providerMock, $this->cacheMock);
+        $this->assertEquals('foo (cache)', $providerCache->getName());
+    }
+
+    public function testMagicFunction()
+    {
+        $this->providerMock->expects($this->once())
+            ->method('getFoo')
+            ->willReturn('foo');
+
+        $providerCache = new ProviderCache($this->providerMock, $this->cacheMock);
+        $this->assertEquals('foo', $providerCache->getFoo());
+    }
+
+    public function testGeocodeMiss()
+    {
+        $query = GeocodeQuery::create('foo');
+        $result = new AddressCollection([Address::createFromArray([])]);
+        $ttl = 4711;
+
+        $this->cacheMock->expects($this->once())
+            ->method('get')
+            ->willReturn(null);
+
+        $this->cacheMock->expects($this->once())
+            ->method('set')
+            ->with($this->anything(), $result, $ttl)
+            ->willReturn(null);
+
+        $this->providerMock->expects($this->once())
+            ->method('geocodeQuery')
+            ->with($query)
+            ->willReturn($result);
+
+        $providerCache = new ProviderCache($this->providerMock, $this->cacheMock, $ttl);
+        $providerCache->geocodeQuery($query);
+    }
+
+    public function testGeocodeHit()
+    {
+        $query = GeocodeQuery::create('foo');
+        $result = new AddressCollection([Address::createFromArray([])]);
+        $ttl = 4711;
+
+        $this->cacheMock->expects($this->once())
+            ->method('get')
+            ->willReturn($result);
+
+        $this->cacheMock->expects($this->never())
+            ->method('set');
+
+        $this->providerMock->expects($this->never())
+            ->method('geocodeQuery');
+
+        $providerCache = new ProviderCache($this->providerMock, $this->cacheMock, $ttl);
+        $providerCache->geocodeQuery($query);
+    }
+
+    public function testReverseMiss()
+    {
+        $query = ReverseQuery::fromCoordinates(1, 2);
+        $result = new AddressCollection([Address::createFromArray([])]);
+        $ttl = 4711;
+
+        $this->cacheMock->expects($this->once())
+            ->method('get')
+            ->willReturn(null);
+
+        $this->cacheMock->expects($this->once())
+            ->method('set')
+            ->with($this->anything(), $result, $ttl)
+            ->willReturn(null);
+
+        $this->providerMock->expects($this->once())
+            ->method('reverseQuery')
+            ->with($query)
+            ->willReturn($result);
+
+        $providerCache = new ProviderCache($this->providerMock, $this->cacheMock, $ttl);
+        $providerCache->reverseQuery($query);
+    }
+
+    public function testReverseHit()
+    {
+        $query = ReverseQuery::fromCoordinates(1, 2);
+        $result = new AddressCollection([Address::createFromArray([])]);
+        $ttl = 4711;
+
+        $this->cacheMock->expects($this->once())
+            ->method('get')
+            ->willReturn($result);
+
+        $this->cacheMock->expects($this->never())
+            ->method('set');
+
+        $this->providerMock->expects($this->never())
+            ->method('reverseQuery');
+
+        $providerCache = new ProviderCache($this->providerMock, $this->cacheMock, $ttl);
+        $providerCache->reverseQuery($query);
+    }
+}
