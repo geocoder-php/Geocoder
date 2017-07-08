ed3af33102280306e751f930e8658955a8325f9c
Deniz <danijela.b1986@gmail.com>
2017-07-08 18:49:41 +0200

    adding address_components parameters (#732)
    * adding address_components paramerets
    
    * cs
    
    * cs
    
    * required changes
    
    * Adding a test for premise address component
    
    * adding establishment parameter
    
    * adding required tests
    
    * cs
    
    * removing unnecessary conditions, making code less complex

--- src/Provider/GoogleMaps/Model/GoogleAddress.php
+++ src/Provider/GoogleMaps/Model/GoogleAddress.php
@@ -37,7 +37,67 @@ final class GoogleAddress extends Address
     /**
      * @var string|null
      */
+    private $streetAddress;
+
+    /**
+     * @var string|null
+     */
+    private $intersection;
+
+    /**
+     * @var string|null
+     */
+    private $political;
+
+    /**
+     * @var string|null
+     */
+    private $colloquialArea;
+
+    /**
+     * @var string|null
+     */
+    private $ward;
+
+    /**
+     * @var string|null
+     */
+    private $neighborhood;
+
+    /**
+     * @var string|null
+     */
+    private $premise;
+
+    /**
+     * @var string|null
+     */
     private $subpremise;
+
+    /**
+     * @var string|null
+     */
+    private $naturalFeature;
+
+    /**
+     * @var string|null
+     */
+    private $airport;
+
+    /**
+     * @var string|null
+     */
+    private $park;
+
+    /**
+     * @var string|null
+     */
+    private $pointOfInterest;
+
+    /**
+     * @var string|null
+     */
+    private $establishment;
 
     /**
      * @param null|string $locationType
@@ -103,6 +163,196 @@ final class GoogleAddress extends Address
     /**
      * @return null|string
      */
+    public function getAirport()
+    {
+        return $this->airport;
+    }
+
+    /**
+     * @param null|string $airport
+     */
+    public function withAirport(string $airport = null)
+    {
+        $new = clone $this;
+        $new->airport = $airport;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getColloquialArea()
+    {
+        return $this->colloquialArea;
+    }
+
+    /**
+     * @param null|string $colloquialArea
+     */
+    public function withColloquialArea(string $colloquialArea = null)
+    {
+        $new = clone $this;
+        $new->colloquialArea = $colloquialArea;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getIntersection()
+    {
+        return $this->intersection;
+    }
+
+    /**
+     * @param null|string $intersection
+     */
+    public function withIntersection(string $intersection = null)
+    {
+        $new = clone $this;
+        $new->intersection = $intersection;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getNaturalFeature()
+    {
+        return $this->naturalFeature;
+    }
+
+    /**
+     * @param null|string $naturalFeature
+     */
+    public function withNaturalFeature(string $naturalFeature = null)
+    {
+        $new = clone $this;
+        $new->naturalFeature = $naturalFeature;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getNeighborhood()
+    {
+        return $this->neighborhood;
+    }
+
+    /**
+     * @param null|string $neighborhood
+     */
+    public function withNeighborhood(string $neighborhood = null)
+    {
+        $new = clone $this;
+        $new->neighborhood = $neighborhood;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPark()
+    {
+        return $this->park;
+    }
+
+    /**
+     * @param null|string $park
+     */
+    public function withPark(string $park = null)
+    {
+        $new = clone $this;
+        $new->park = $park;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPointOfInterest()
+    {
+        return $this->pointOfInterest;
+    }
+
+    /**
+     * @param null|string $pointOfInterest
+     */
+    public function withPointOfInterest(string $pointOfInterest = null)
+    {
+        $new = clone $this;
+        $new->pointOfInterest = $pointOfInterest;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPolitical()
+    {
+        return $this->political;
+    }
+
+    /**
+     * @param null|string $political
+     */
+    public function withPolitical(string $political = null)
+    {
+        $new = clone $this;
+        $new->political = $political;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPremise()
+    {
+        return $this->premise;
+    }
+
+    /**
+     * @param null|string $premise
+     */
+    public function withPremise($premise = null)
+    {
+        $new = clone $this;
+        $new->premise = $premise;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getStreetAddress()
+    {
+        return $this->streetAddress;
+    }
+
+    /**
+     * @param null|string $streetAddress
+     */
+    public function withStreetAddress(string $streetAddress = null)
+    {
+        $new = clone $this;
+        $new->streetAddress = $streetAddress;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
     public function getSubpremise()
     {
         return $this->subpremise;
@@ -115,6 +365,44 @@ final class GoogleAddress extends Address
     {
         $new = clone $this;
         $new->subpremise = $subpremise;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getWard()
+    {
+        return $this->ward;
+    }
+
+    /**
+     * @param null|string $ward
+     */
+    public function withWard(string $ward = null)
+    {
+        $new = clone $this;
+        $new->ward = $ward;
+
+        return $new;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getEstablishment()
+    {
+        return $this->establishment;
+    }
+
+    /**
+     * @param null|string $ward
+     */
+    public function withEstablishment(string $establishment = null)
+    {
+        $new = clone $this;
+        $new->establishment = $establishment;
 
         return $new;
     }
