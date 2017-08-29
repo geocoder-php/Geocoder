e96123975d707fb1168f7e901bd3ec63a9bd66e7
Srihari Thalla <srihari@unwiredlabs.com>
2017-08-29 15:34:19 +0530

    Add LocationIQ geocoder provider

---
+++ src/Provider/LocationIQ/Tests/IntegrationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\LocationIQ\Tests;
+
+use Geocoder\IntegrationTest\ProviderIntegrationTest;
+use Geocoder\Provider\LocationIQ\LocationIQ;
+use Http\Client\HttpClient;
+
+/**
+ * @author Srihari Thalla <srihari@unwiredlabs.com>
+ */
+class IntegrationTest extends ProviderIntegrationTest
+{
+    protected function createProvider(HttpClient $httpClient)
+    {
+        return new LocationIQ($httpClient, $this->getApiKey());
+    }
+
+    protected function getCacheDir()
+    {
+        return __DIR__.'/.cached_responses';
+    }
+
+    protected function getApiKey()
+    {
+        return $_SERVER['LOCATIONIQ_API_KEY'];
+    }
+}
---
+++ src/Provider/LocationIQ/Tests/LocationIQTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\LocationIQ\Tests;
+
+use Geocoder\Collection;
+use Geocoder\IntegrationTest\BaseTestCase;
+use Geocoder\Location;
+use Geocoder\Provider\LocationIQ\LocationIQ;
+use Geocoder\Query\GeocodeQuery;
+use Geocoder\Query\ReverseQuery;
+
+class LocationIQTest extends BaseTestCase
+{
+    protected function getCacheDir()
+    {
+        return __DIR__.'/.cached_responses';
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\InvalidServerResponse
+     */
+    public function testGeocodeWithAddressGetsEmptyContent()
+    {
+        $provider = new LocationIQ($this->getMockedHttpClient('<foo></foo>'), $_SERVER['LOCATIONIQ_API_KEY']);
+        $provider->geocodeQuery(GeocodeQuery::create('Läntinen Pitkäkatu 35, Turku'));
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\InvalidServerResponse
+     */
+    public function testGeocodeWithAddressGetsEmptyXML()
+    {
+        $emptyXML = <<<'XML'
+<?xml version="1.0" encoding="utf-8"?><searchresults_empty></searchresults_empty>
+XML;
+
+        $provider = new LocationIQ($this->getMockedHttpClient($emptyXML), $_SERVER['LOCATIONIQ_API_KEY']);
+        $provider->geocodeQuery(GeocodeQuery::create('Läntinen Pitkäkatu 35, Turku'));
+    }
+
+    public function testReverseWithCoordinatesGetsError()
+    {
+        $errorXml = <<<'XML'
+<?xml version="1.0" encoding="UTF-8" ?>
+<reversegeocode querystring='format=xml&amp;lat=-80.000000&amp;lon=-170.000000&amp;addressdetails=1'>
+    <error>Unable to geocode</error>
+</reversegeocode>
+XML;
+
+        $provider = new LocationIQ($this->getMockedHttpClient($errorXml), $_SERVER['LOCATIONIQ_API_KEY']);
+
+        $result = $provider->reverseQuery(ReverseQuery::fromCoordinates(-80.000000, -170.000000));
+
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertEquals(0, $result->count());
+    }
+
+    public function testGetNodeStreetName()
+    {
+        $provider = new LocationIQ($this->getHttpClient($_SERVER['LOCATIONIQ_API_KEY']), $_SERVER['LOCATIONIQ_API_KEY']);
+        $results = $provider->reverseQuery(ReverseQuery::fromCoordinates(48.86, 2.35));
+
+        $this->assertInstanceOf('Geocoder\Model\AddressCollection', $results);
+        $this->assertCount(1, $results);
+
+        /** @var Location $result */
+        $result = $results->first();
+        $this->assertInstanceOf('\Geocoder\Model\Address', $result);
+        $this->assertEquals('Rue Quincampoix', $result->getStreetName());
+    }
+}
