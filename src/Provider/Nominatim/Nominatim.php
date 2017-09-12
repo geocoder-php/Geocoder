775a1144b76f7ee43c50f3244c8e482670abfc69
Sébastien <sebastien@yoopies.com>
2017-09-12 16:57:16 +0200

    NominatimProvider: get locality from city, then town, then village, then hamlet

--- src/Provider/Nominatim/Nominatim.php
+++ src/Provider/Nominatim/Nominatim.php
@@ -138,10 +138,20 @@ final class Nominatim extends AbstractHttpProvider implements Provider
         if (!empty($postalCode)) {
             $postalCode = current(explode(';', $postalCode));
         }
+
+        $localityFields = ['city', 'town', 'village', 'hamlet'];
+        foreach ($localityFields as $localityField) {
+            $localityFieldContent = $this->getNodeValue($addressNode->getElementsByTagName($localityField));
+            if (!empty($localityFieldContent)) {
+                $builder->setLocality($localityFieldContent);
+
+                break;
+            }
+        }
+
         $builder->setPostalCode($postalCode);
         $builder->setStreetName($this->getNodeValue($addressNode->getElementsByTagName('road')) ?: $this->getNodeValue($addressNode->getElementsByTagName('pedestrian')));
         $builder->setStreetNumber($this->getNodeValue($addressNode->getElementsByTagName('house_number')));
-        $builder->setLocality($this->getNodeValue($addressNode->getElementsByTagName('city')));
         $builder->setSubLocality($this->getNodeValue($addressNode->getElementsByTagName('suburb')));
         $builder->setCountry($this->getNodeValue($addressNode->getElementsByTagName('country')));
 
