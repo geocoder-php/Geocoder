3600a9293dd1e8ca685b9d85378c3897ab4dcc9a
Stadly <magnar@myrtveit.com>
2018-01-26 17:54:01 +0100

    Return empty collection if address was not found
    If the location of the ip address cannot be determined by GeoPlugin, return an empty AddressCollection instead of dummy data. This is particularly useful when using the Chain provider.

--- src/Provider/GeoPlugin/GeoPlugin.php
+++ src/Provider/GeoPlugin/GeoPlugin.php
@@ -85,6 +85,17 @@ final class GeoPlugin extends AbstractHttpProvider implements Provider
             return new AddressCollection([]);
         }
 
+        // Return empty collection if address was not found
+        if ($json['geoplugin_regionName'] === ''
+        &&  $json['geoplugin_regionCode'] === ''
+        &&  $json['geoplugin_city'] === ''
+        &&  $json['geoplugin_countryName'] === ''
+        &&  $json['geoplugin_countryCode'] === ''
+        &&  $json['geoplugin_latitude'] === '0'
+        &&  $json['geoplugin_longitude'] === '0') {
+            return new AddressCollection([]);
+        }
+
         $data = array_filter($json);
 
         $adminLevels = [];
