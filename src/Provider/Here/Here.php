75ba5b25518f625d3a415bd545dc370c2b36a17c
Sheub <sebastien@sheub.eu>
2018-04-17 21:43:47 +0200

    HereMaps->Here

--- src/Provider/Here/Here.php
+++ src/Provider/Here/Here.php
@@ -72,7 +72,7 @@ final class Here extends AbstractHttpProvider implements Provider
     {
         // This API doesn't handle IPs
         if (filter_var($query->getText(), FILTER_VALIDATE_IP)) {
-            throw new UnsupportedOperation('The HereMaps provider does not support IP addresses, only street addresses.');
+            throw new UnsupportedOperation('The Here provider does not support IP addresses, only street addresses.');
         }
 
         $url = sprintf(self::GEOCODE_ENDPOINT_URL, $this->appId, $this->appCode, rawurlencode($query->getText()));
