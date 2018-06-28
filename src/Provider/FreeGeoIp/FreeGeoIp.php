4280cfb3be6fc167fe112cb1c39b570b78cdcc06
Stadly <magnar@myrtveit.com>
2018-06-28 14:21:32 +0200

    Use AbstractHttpProvider in FreeGeoIp (#865)
    * Use AbstractHttpProvider in FreeGeoIp
    
    Use `getRequest` and `getParsedResponse` from `AbstractHttpProvider` instead of duplicating code in `FreeGeoIp`. See https://github.com/geocoder-php/Geocoder/pull/864.
    
    * Remove unnecessary uses
    
    * Update dependencies

--- src/Provider/FreeGeoIp/FreeGeoIp.php
+++ src/Provider/FreeGeoIp/FreeGeoIp.php
@@ -13,9 +13,6 @@ declare(strict_types=1);
 namespace Geocoder\Provider\FreeGeoIp;
 
 use Geocoder\Collection;
-use Geocoder\Exception\InvalidCredentials;
-use Geocoder\Exception\InvalidServerResponse;
-use Geocoder\Exception\QuotaExceeded;
 use Geocoder\Exception\UnsupportedOperation;
 use Geocoder\Model\AddressBuilder;
 use Geocoder\Model\AddressCollection;
@@ -60,29 +57,13 @@ final class FreeGeoIp extends AbstractHttpProvider implements Provider
             return new AddressCollection([$this->getLocationForLocalhost()]);
         }
 
-        $url = sprintf($this->baseUrl, $address);
-        $request = $this->getMessageFactory()->createRequest('GET', $url);
+        $request = $this->getRequest(sprintf($this->baseUrl, $address));
 
         if (null !== $query->getLocale()) {
             $request = $request->withHeader('Accept-Language', $query->getLocale());
         }
 
-        $response = $this->getHttpClient()->sendRequest($request);
-
-        $statusCode = $response->getStatusCode();
-        if (401 === $statusCode || 403 === $statusCode) {
-            throw new InvalidCredentials();
-        } elseif (429 === $statusCode) {
-            throw new QuotaExceeded();
-        } elseif ($statusCode >= 300) {
-            throw InvalidServerResponse::create($url, $statusCode);
-        }
-
-        $body = (string) $response->getBody();
-        if (empty($body)) {
-            throw InvalidServerResponse::emptyResponse($url);
-        }
-
+        $body = $this->getParsedResponse($request);
         $data = json_decode($body, true);
 
         // Return empty collection if address was not found
