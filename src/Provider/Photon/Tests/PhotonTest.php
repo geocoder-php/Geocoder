7119d34f1d598a6c80ee3fc8a136fd0e28842dc4
Jonathan Beliën <jbe@geo6.be>
2018-11-23 16:51:02 +0100

    Add reverse geocoding test

--- src/Provider/Photon/Tests/PhotonTest.php
+++ src/Provider/Photon/Tests/PhotonTest.php
@@ -56,7 +56,7 @@ class PhotonTest extends BaseTestCase
         $provider->geocodeQuery(GeocodeQuery::create('::ffff:88.188.221.14'));
     }
 
-    public function testGeocodeWithRealAddress()
+    public function testGeocodeQuery()
     {
         $provider = Photon::withKomootServer($this->getHttpClient());
         $results = $provider->geocodeQuery(GeocodeQuery::create('35 avenue jean de bologne 1020 bruxelles'));
@@ -73,10 +73,35 @@ class PhotonTest extends BaseTestCase
         $this->assertEquals('Avenue Jean de Bologne - Jean de Bolognelaan', $result->getStreetName());
         $this->assertEquals('1020', $result->getPostalCode());
         $this->assertEquals('Ville de Bruxelles - Stad Brussel', $result->getLocality());
+        $this->assertEquals('Belgium', $result->getCountry());
 
         $this->assertEquals(220754533, $result->getOSMId());
         $this->assertEquals('W', $result->getOSMType());
         $this->assertEquals('building', $result->getOSMTag()->key);
         $this->assertEquals('yes', $result->getOSMTag()->value);
     }
+
+    public function testReverseQuery()
+    {
+        $provider = Photon::withKomootServer($this->getHttpClient());
+        $results = $provider->reverseQuery(ReverseQuery::fromCoordinates(52, 10));
+
+        $this->assertInstanceOf('Geocoder\Model\AddressCollection', $results);
+        $this->assertCount(1, $results);
+
+        /** @var \Geocoder\Model\Address $result */
+        $result = $results->first();
+        $this->assertInstanceOf('\Geocoder\Model\Address', $result);
+        $this->assertEquals(52.004657, $result->getCoordinates()->getLatitude(), '', 0.00001);
+        $this->assertEquals(10.012148, $result->getCoordinates()->getLongitude(), '', 0.00001);
+        $this->assertEquals('31195', $result->getPostalCode());
+        $this->assertEquals('Lamspringe', $result->getLocality());
+        $this->assertEquals('Germany', $result->getCountry());
+
+        $this->assertEquals(15219847, $result->getOSMId());
+        $this->assertEquals('W', $result->getOSMType());
+        $this->assertEquals('highway', $result->getOSMTag()->key);
+        $this->assertEquals('tertiary', $result->getOSMTag()->value);
+    }
+
 }
