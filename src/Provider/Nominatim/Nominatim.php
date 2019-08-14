e512167ff31589c016fe82a455ecb73eedd70688
Eileen McNaughton <eileen@mcnaughty.com>
2019-08-14 14:36:42 +1200

    Formatting (doing this through WEB UI is not much fun)

--- src/Provider/Nominatim/Nominatim.php
+++ src/Provider/Nominatim/Nominatim.php
@@ -233,7 +233,7 @@ final class Nominatim extends AbstractHttpProvider implements Provider
         if (!empty($place->osm_id)) {
             $location = $location->withOSMId(intval($place->osm_id));
         }
-        if (!empty($place->osm_type)) {    
+        if (!empty($place->osm_type)) {
             $location = $location->withOSMType($place->osm_type);
         }
         $location = $location->withDisplayName($place->display_name);
