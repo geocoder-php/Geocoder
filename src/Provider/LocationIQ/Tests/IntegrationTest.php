c550dcbeecd0389a0518a19c5c12c49e45778104
Jonathan Beliën <jbelien@users.noreply.github.com>
2020-01-08 21:53:26 +0100

    Update IntegrationTest.php
    Apply fixes from StyleCI

--- src/Provider/LocationIQ/Tests/IntegrationTest.php
+++ src/Provider/LocationIQ/Tests/IntegrationTest.php
@@ -24,6 +24,7 @@ class IntegrationTest extends ProviderIntegrationTest
     ];
 
     protected $testIpv4 = false;
+
     protected $testIpv6 = false;
 
     protected function createProvider(HttpClient $httpClient)
@@ -33,7 +34,7 @@ class IntegrationTest extends ProviderIntegrationTest
 
     protected function getCacheDir()
     {
-        return __DIR__ . '/.cached_responses';
+        return __DIR__.'/.cached_responses';
     }
 
     protected function getApiKey()
