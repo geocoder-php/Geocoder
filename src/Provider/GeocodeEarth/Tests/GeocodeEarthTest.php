acd5a35155e293cb04cea6d29022168dd6d99e73
Jonathan Beliën <dev@jbelien.be>
2020-04-22 18:46:32 +0200

    Update GeocodeEarthTest.php
    Fix testGeocodeNoBounds.

--- src/Provider/GeocodeEarth/Tests/GeocodeEarthTest.php
+++ src/Provider/GeocodeEarth/Tests/GeocodeEarthTest.php
@@ -203,20 +203,17 @@ class GeocodeEarthTest extends BaseTestCase
         $results = $provider->geocodeQuery(GeocodeQuery::create('dworzec centralny'));
 
         $this->assertInstanceOf('Geocoder\Model\AddressCollection', $results);
-        $this->assertCount(2, $results);
+        $this->assertCount(5, $results);
 
         /** @var \Geocoder\Model\Address $result */
         $result = $results->first();
         $this->assertInstanceOf('\Geocoder\Model\Address', $result);
         $this->assertEquals(52.230428, $result->getCoordinates()->getLatitude(), '', 0.01);
         $this->assertEquals(21.004552, $result->getCoordinates()->getLongitude(), '', 0.01);
-        $this->assertEquals('Warszawa', $result->getLocality());
-        $this->assertEquals('Polska', $result->getCountry()->getName());
+        $this->assertEquals('Warsaw', $result->getLocality());
+        $this->assertEquals('Poland', $result->getCountry()->getName());
         $this->assertEquals('POL', $result->getCountry()->getCode());
-        $this->assertNull($result->getBounds()->getEast());
-        $this->assertNull($result->getBounds()->getNorth());
-        $this->assertNull($result->getBounds()->getSouth());
-        $this->assertNull($result->getBounds()->getWest());
+        $this->assertNull($result->getBounds());
     }
 
     /**
