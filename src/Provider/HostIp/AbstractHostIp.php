8c3c38a2a3825fbe612b04be13fa3d453283092c
Oleg Andreyev <oleg@andreyev.lv>
2020-10-16 09:37:47 +0300

    Created HostIpXml all alternative to HostIp (#1057)
    * Created HostIpXml all alternative to HostIp(json) because JSON does not have lat,lng anymore.
    
    !squash code style fixes
    
    * added require to provider composer.json
    
    * updated function fqn
    
    * added interface to AbstractHostIp

---
+++ src/Provider/HostIp/AbstractHostIp.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\HostIp;
+
+use Geocoder\Collection;
+use Geocoder\Exception\UnsupportedOperation;
+use Geocoder\Http\Provider\AbstractHttpProvider;
+use Geocoder\Model\Address;
+use Geocoder\Model\AddressCollection;
+use Geocoder\Query\GeocodeQuery;
+use Geocoder\Query\ReverseQuery;
+
+/**
+ * @author William Durand <william.durand1@gmail.com>
+ * @author Oleg Andreyev <oleg@andreyev.lv>
+ */
+abstract class AbstractHostIp extends AbstractHttpProvider implements \Geocoder\Provider\Provider
+{
+    abstract protected function executeQuery(string $url): AddressCollection;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function geocodeQuery(GeocodeQuery $query): Collection
+    {
+        $address = $query->getText();
+        if (!filter_var($address, FILTER_VALIDATE_IP)) {
+            throw new UnsupportedOperation('The '.get_class($this).' provider does not support Street addresses.');
+        }
+
+        // This API does not support IPv6
+        if (filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+            throw new UnsupportedOperation('The HostIp provider does not support IPv6 addresses.');
+        }
+
+        if ('127.0.0.1' === $address) {
+            return new AddressCollection([$this->getLocationForLocalhost()]);
+        }
+
+        $url = sprintf(static::ENDPOINT_URL, $address);
+
+        return $this->executeQuery($url);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseQuery(ReverseQuery $query): Collection
+    {
+        throw new UnsupportedOperation('The HostIp provider is not able to do reverse geocoding.');
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return bool
+     */
+    protected function isUnknownLocation(array $data): bool
+    {
+        return empty($data['lat'])
+            && empty($data['lng'])
+            && '(Unknown City?)' === $data['city']
+            && '(Unknown Country?)' === $data['country_name']
+            && 'XX' === $data;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return bool
+     */
+    protected function isPrivateLocation(array $data): bool
+    {
+        return empty($data['lat'])
+            && empty($data['lng'])
+            && '(Private Address)' === $data['city']
+            && '(Private Address)' === $data['country_name']
+            && 'XX' === $data;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return AddressCollection
+     */
+    protected function prepareAddressCollection(array $data): AddressCollection
+    {
+        // Return empty collection if address was not found
+        if ($this->isUnknownLocation($data)) {
+            return new AddressCollection([]);
+        }
+
+        // Return empty collection if address was not found
+        if ($this->isPrivateLocation($data)) {
+            return new AddressCollection([]);
+        }
+
+        return new AddressCollection([
+            Address::createFromArray([
+                'providedBy' => $this->getName(),
+                'latitude' => $data['lat'] ?? null,
+                'longitude' => $data['lng'] ?? null,
+                'locality' => $data['city'],
+                'country' => $data['country_name'],
+                'countryCode' => $data['country_code'],
+            ]),
+        ]);
+    }
+}
