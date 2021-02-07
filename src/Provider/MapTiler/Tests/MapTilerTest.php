4c2c1b05524d5a857fcfe2420701bb7d082bb129
Jonathan Beliën <dev@jbelien.be>
2021-02-07 17:10:17 +0100

    Update tests

--- src/Provider/MapTiler/Tests/MapTilerTest.php
+++ src/Provider/MapTiler/Tests/MapTilerTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Geocoder\Provider\MapTiler\Tests;
 
 use Geocoder\IntegrationTest\BaseTestCase;
+use Geocoder\Model\Bounds;
 use Geocoder\Query\GeocodeQuery;
 use Geocoder\Query\ReverseQuery;
 use Geocoder\Provider\MapTiler\MapTiler;
@@ -66,32 +67,52 @@ class MapTilerTest extends BaseTestCase
         $provider->geocodeQuery(GeocodeQuery::create('::ffff:88.188.221.14'));
     }
 
-    public function testGeocodeQuery()
+    public function testGeocodeQueryStreet()
     {
         if (!isset($_SERVER['MAPTILER_KEY'])) {
             $this->markTestSkipped('You need to configure the MAPTILER_KEY value in phpunit.xml');
         }
 
-        $provider = new MapTiler($this->getMockedHttpClient(), $_SERVER['MAPTILER_KEY']);
-        $results = $provider->geocodeQuery(GeocodeQuery::create('Avenue Gambetta Paris France'));
+        $query = GeocodeQuery::create('Avenue Gambetta, Paris, France');
+        $query = $query->withBounds(new Bounds(2.293039, 48.821036, 2.406336, 48.894899));
+
+        $provider = new MapTiler($this->getHttpClient($_SERVER['MAPTILER_KEY']), $_SERVER['MAPTILER_KEY']);
+        $results = $provider->geocodeQuery($query);
 
         $this->assertInstanceOf('Geocoder\Model\AddressCollection', $results);
-        $this->assertCount(1, $results);
+        // $this->assertCount(1, $results);
 
         /** @var \Geocoder\Model\Address $result */
         $result = $results->first();
         $this->assertInstanceOf('\Geocoder\Model\Address', $result);
-        $this->assertEquals(48.8631927, $result->getCoordinates()->getLatitude(), '', 0.00001);
-        $this->assertEquals(2.3890894, $result->getCoordinates()->getLongitude(), '', 0.00001);
+        $this->assertEquals(48.8658863, $result->getCoordinates()->getLatitude(), '', 0.00001);
+        $this->assertEquals(2.3993232, $result->getCoordinates()->getLongitude(), '', 0.00001);
         $this->assertEquals('Avenue Gambetta', $result->getStreetName());
-        $this->assertEquals('75020', $result->getPostalCode());
         $this->assertEquals('Paris', $result->getLocality());
         $this->assertEquals('France', $result->getCountry());
+    }
 
-        // $this->assertEquals(1988097192, $result->getOSMId());
-        // $this->assertEquals('N', $result->getOSMType());
-        // $this->assertEquals('place', $result->getOSMTag()->key);
-        // $this->assertEquals('house', $result->getOSMTag()->value);
+    public function testGeocodeQueryCity()
+    {
+        if (!isset($_SERVER['MAPTILER_KEY'])) {
+            $this->markTestSkipped('You need to configure the MAPTILER_KEY value in phpunit.xml');
+        }
+
+        $query = GeocodeQuery::create('Paris, France');
+
+        $provider = new MapTiler($this->getHttpClient($_SERVER['MAPTILER_KEY']), $_SERVER['MAPTILER_KEY']);
+        $results = $provider->geocodeQuery($query);
+
+        $this->assertInstanceOf('Geocoder\Model\AddressCollection', $results);
+        // $this->assertCount(1, $results);
+
+        /** @var \Geocoder\Model\Address $result */
+        $result = $results->get(1);
+        $this->assertInstanceOf('\Geocoder\Model\Address', $result);
+        $this->assertEquals(48.85881, $result->getCoordinates()->getLatitude(), '', 0.00001);
+        $this->assertEquals(2.320031, $result->getCoordinates()->getLongitude(), '', 0.00001);
+        $this->assertEquals('Paris', $result->getLocality());
+        $this->assertEquals('France', $result->getCountry());
     }
 
     public function testReverseQuery()
@@ -100,24 +121,20 @@ class MapTilerTest extends BaseTestCase
             $this->markTestSkipped('You need to configure the MAPTILER_KEY value in phpunit.xml');
         }
 
-        $provider = new MapTiler($this->getMockedHttpClient(), $_SERVER['MAPTILER_KEY']);
-        $results = $provider->reverseQuery(ReverseQuery::fromCoordinates(52, 10));
+        $query = ReverseQuery::fromCoordinates(47.3774434, 8.528509);
+
+        $provider = new MapTiler($this->getHttpClient($_SERVER['MAPTILER_KEY']), $_SERVER['MAPTILER_KEY']);
+        $results = $provider->reverseQuery($query);
 
         $this->assertInstanceOf('Geocoder\Model\AddressCollection', $results);
-        $this->assertCount(1, $results);
+        // $this->assertCount(1, $results);
 
         /** @var \Geocoder\Model\Address $result */
         $result = $results->first();
         $this->assertInstanceOf('\Geocoder\Model\Address', $result);
-        $this->assertEquals(51.9982968, $result->getCoordinates()->getLatitude(), '', 0.00001);
-        $this->assertEquals(9.998645, $result->getCoordinates()->getLongitude(), '', 0.00001);
-        $this->assertEquals('31195', $result->getPostalCode());
-        $this->assertEquals('Lamspringe', $result->getLocality());
-        $this->assertEquals('Deutschland', $result->getCountry());
-
-        // $this->assertEquals(693697564, $result->getOSMId());
-        // $this->assertEquals('N', $result->getOSMType());
-        // $this->assertEquals('tourism', $result->getOSMTag()->key);
-        // $this->assertEquals('information', $result->getOSMTag()->value);
+        $this->assertEquals(47.3774434, $result->getCoordinates()->getLatitude(), '', 0.00001);
+        $this->assertEquals(8.528509, $result->getCoordinates()->getLongitude(), '', 0.00001);
+        $this->assertEquals('Zurich', $result->getLocality());
+        $this->assertEquals('Switzerland', $result->getCountry());
     }
 }
