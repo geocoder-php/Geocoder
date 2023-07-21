734c5c52fbcba4bc12cbda07b58d902a79d47891
Jonathan Beliën <dev@jbelien.be>
2023-07-21 16:20:43 +0200

    [WIP] Apply PHPStan fixes

--- src/Common/Dumper/Dumper.php
+++ src/Common/Dumper/Dumper.php
@@ -22,6 +22,8 @@ interface Dumper
     /**
      * Dumps an `Location` object as a string representation of
      * the implemented format.
+     *
+     * @return string|array
      */
-    public function dump(Location $location): mixed;
+    public function dump(Location $location);
 }
