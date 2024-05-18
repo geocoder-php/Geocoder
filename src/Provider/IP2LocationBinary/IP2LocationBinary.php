b5024731a4277e743e16d3db8b12c85f1570f77f
Boris Momčilović <boris@firstbeatmedia.com>
2024-05-18 22:08:48 +0200

    Utf8 encode (#1225)
    * provider: IP2LocationBinary: replace utf8_encode with mb_convert_encoding
    
    * provider: MaxMindBinary: replace utf8_encode with mb_convert_encoding

--- src/Provider/IP2LocationBinary/IP2LocationBinary.php
+++ src/Provider/IP2LocationBinary/IP2LocationBinary.php
@@ -81,9 +81,9 @@ final class IP2LocationBinary extends AbstractProvider implements Provider
             Address::createFromArray([
                 'providedBy' => $this->getName(),
                 'countryCode' => $records['countryCode'],
-                'country' => null === $records['countryName'] ? null : utf8_encode($records['countryName']),
+                'country' => null === $records['countryName'] ? null : mb_convert_encoding($records['countryName'], 'UTF-8', 'ISO-8859-1'),
                 'adminLevels' => $adminLevels,
-                'locality' => null === $records['cityName'] ? null : utf8_encode($records['cityName']),
+                'locality' => null === $records['cityName'] ? null : mb_convert_encoding($records['cityName'], 'UTF-8', 'ISO-8859-1'),
                 'latitude' => $records['latitude'],
                 'longitude' => $records['longitude'],
                 'postalCode' => $records['zipCode'],
