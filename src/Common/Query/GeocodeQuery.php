fa1b5ce6fae62013e96a259ad81498c45fbd0cab
AT <atrinh@servicecore.com>
2024-07-02 11:54:44 -0600

    Revert "Updated geocode query to allow support for qualified queries"
    This reverts commit 72a671d23674f48ebb970763b5bd889ec771068d.

--- src/Common/Query/GeocodeQuery.php
+++ src/Common/Query/GeocodeQuery.php
@@ -24,7 +24,7 @@ final class GeocodeQuery implements Query
     /**
      * The address or text that should be geocoded.
      *
-     * @var string|null
+     * @var string
      */
     private $text;
 
@@ -48,7 +48,7 @@ final class GeocodeQuery implements Query
      */
     private $data = [];
 
-    private function __construct(?string $text)
+    private function __construct(string $text)
     {
         if ('' === $text) {
             throw new InvalidArgument('Geocode query cannot be empty');
@@ -57,7 +57,7 @@ final class GeocodeQuery implements Query
         $this->text = $text;
     }
 
-    public static function create(?string $text): self
+    public static function create(string $text): self
     {
         return new self($text);
     }
@@ -102,7 +102,7 @@ final class GeocodeQuery implements Query
         return $new;
     }
 
-    public function getText(): ?string
+    public function getText(): string
     {
         return $this->text;
     }
