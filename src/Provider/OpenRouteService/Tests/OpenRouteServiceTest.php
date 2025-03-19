412604e5b6577705b7e1849d6f24296950f9f586
Jonathan Beliën <jonathan.belien@nccn.fgov.be>
2025-03-19 17:00:28 +0100

    Update OpenRouteService tests

--- src/Provider/OpenRouteService/Tests/OpenRouteServiceTest.php
+++ src/Provider/OpenRouteService/Tests/OpenRouteServiceTest.php
@@ -86,9 +86,9 @@ class OpenRouteServiceTest extends BaseTestCase
         $this->assertEquals('Ffrances Passage', $result->getStreetName());
         $this->assertEquals('LA1 1UG', $result->getPostalCode());
         $this->assertEquals('Lancaster', $result->getLocality());
-        $this->assertCount(5, $result->getAdminLevels());
-        $this->assertEquals('Lancashire', $result->getAdminLevels()->get(1)->getName());
-        $this->assertEquals('England', $result->getAdminLevels()->get(4)->getName());
+        $this->assertCount(4, $result->getAdminLevels());
+        $this->assertEquals('Lancashire', $result->getAdminLevels()->get(3)->getName());
+        $this->assertEquals('England', $result->getAdminLevels()->get(1)->getName());
         $this->assertEquals('United Kingdom', $result->getCountry()->getName());
         $this->assertEquals('GBR', $result->getCountry()->getCode());
     }
@@ -140,7 +140,7 @@ class OpenRouteServiceTest extends BaseTestCase
         $this->assertEqualsWithDelta(18.393428, $result->getCoordinates()->getLatitude(), 0.01);
         $this->assertEqualsWithDelta(-78.107687, $result->getCoordinates()->getLongitude(), 0.01);
         $this->assertNull($result->getLocality());
-        $this->assertCount(2, $result->getAdminLevels());
+        $this->assertCount(1, $result->getAdminLevels());
         $this->assertEquals('Hanover', $result->getAdminLevels()->get(1)->getName());
         $this->assertEquals('Jamaica', $result->getCountry()->getName());
 
@@ -150,7 +150,7 @@ class OpenRouteServiceTest extends BaseTestCase
         $this->assertEqualsWithDelta(39.192889999999998, $result->getCoordinates()->getLatitude(), 0.01);
         $this->assertEqualsWithDelta(-76.724140000000006, $result->getCoordinates()->getLongitude(), 0.01);
         $this->assertEquals('Hanover', $result->getLocality());
-        $this->assertCount(4, $result->getAdminLevels());
+        $this->assertCount(3, $result->getAdminLevels());
         $this->assertEquals('Hanover', $result->getAdminLevels()->get(3)->getName());
         $this->assertEquals('United States', $result->getCountry()->getName());
     }
@@ -176,10 +176,10 @@ class OpenRouteServiceTest extends BaseTestCase
         $this->assertEquals('Kalbacher Hauptstraße', $result->getStreetName());
         $this->assertEquals(60437, $result->getPostalCode());
         $this->assertEquals('Frankfurt', $result->getLocality());
-        $this->assertCount(4, $result->getAdminLevels());
-        $this->assertEquals('Frankfurt', $result->getAdminLevels()->get(2)->getName());
+        $this->assertCount(5, $result->getAdminLevels());
+        $this->assertEquals('Frankfurt', $result->getAdminLevels()->get(4)->getName());
         $this->assertEquals('Hesse', $result->getAdminLevels()->get(1)->getName());
-        $this->assertNull($result->getAdminLevels()->get(1)->getCode());
+        $this->assertEquals('HE', $result->getAdminLevels()->get(1)->getCode());
         $this->assertEquals('Germany', $result->getCountry()->getName());
         $this->assertEquals('DEU', $result->getCountry()->getCode());
     }
