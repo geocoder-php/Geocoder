0f474059511b7fb095dc3ad79c4e9f3c803c05bc
Frederik Buchlák <30214087+fbuchlak@users.noreply.github.com>
2025-03-23 09:29:28 +0100

    feat: [Photon] support lat/lon in geocode query (#1248)
    * feat: [Photon] support lat/lon in geocode query
    
    * style: [photon] use class name resolution in test

--- src/Provider/Photon/Tests/PhotonTest.php
+++ src/Provider/Photon/Tests/PhotonTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Geocoder\Provider\Photon\Tests;
 
 use Geocoder\IntegrationTest\BaseTestCase;
+use Geocoder\Model\AddressCollection;
 use Geocoder\Provider\Photon\Model\PhotonAddress;
 use Geocoder\Provider\Photon\Photon;
 use Geocoder\Query\GeocodeQuery;
@@ -133,6 +134,25 @@ class PhotonTest extends BaseTestCase
         }
         $this->assertGreaterThan(0, $countMuseums);
         $this->assertGreaterThan(0, $countGalleries);
+    }
+
+    public function testGeocodeQueryWithLatLon(): void
+    {
+        $provider = Photon::withKomootServer($this->getHttpClient());
+
+        $query = GeocodeQuery::create('Paris')->withLimit(1);
+        $results = $provider->geocodeQuery($query);
+        $this->assertInstanceOf(AddressCollection::class, $results);
+        $this->assertCount(1, $results);
+        $this->assertEquals('France', $results->first()->getCountry());
+
+        $query = $query
+            ->withData('lat', 33.661426)
+            ->withData('lon', -95.556321);
+        $results = $provider->geocodeQuery($query);
+        $this->assertInstanceOf(AddressCollection::class, $results);
+        $this->assertCount(1, $results);
+        $this->assertEquals('United States', $results->first()->getCountry());
     }
 
     public function testReverseQuery(): void
