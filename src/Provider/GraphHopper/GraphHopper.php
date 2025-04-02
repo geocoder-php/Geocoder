ecb27076aaed3957a4154f005a17f3a77976ae84
bisleadity <177923681+bisleadity@users.noreply.github.com>
2025-04-02 13:19:06 +0200

    Revert "[GraphHopper] Add GraphHopperProvider enum for provider options of GraphHopper geocoding api"
    This reverts commit 320b5d783d1cda50cc6b7434d3bb4e24adfe45a8.

--- src/Provider/GraphHopper/GraphHopper.php
+++ src/Provider/GraphHopper/GraphHopper.php
@@ -75,7 +75,7 @@ final class GraphHopper extends AbstractHttpProvider implements Provider
         }
 
         $provider = $query->getData('provider');
-        if (is_string($provider) && GraphHopperProvider::tryFrom($provider)) {
+        if (is_string($provider) && '' !== $provider) {
             $url .= sprintf('&provider=%s', urlencode($provider));
         }
 
